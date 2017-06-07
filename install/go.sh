c749b96364b7f3e511b66556e56deebf0cd5d8b7
Thibault Laurens <laurens.thibault@gmail.com>
2017-06-07 23:59:22 +0100

    update go config

--- install/go.sh
+++ install/go.sh
@@ -12,9 +12,17 @@ brew install go
 echo "installing go packages..."
 packages=(
     golang.org/x/tools/cmd/godoc
+    golang.org/x/tools/cmd/guru
+    golang.org/x/tools/cmd/gorename
+    golang.org/x/tools/cmd/goimports
     github.com/nsf/gocode
     github.com/k0kubun/pp
     github.com/motemen/gore
+    github.com/rogpeppe/godef
+    github.com/alecthomas/gometalinter
+    github.com/mailgun/godebug
 )
 
-go get -u "${packages[@]}"
+go get -u -v "${packages[@]}"
+
+gometalinter --install --update
