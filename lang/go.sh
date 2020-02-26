8c5f85a5ec8cabde87d397b9ec14aa3db3e4c5f0
Thibault Laurens <laurens.thibault@gmail.com>
2020-02-26 09:59:01 +0100

    update go packages to install

--- lang/go.sh
+++ lang/go.sh
@@ -24,11 +24,20 @@ packages=(
     golang.org/x/tools/cmd/guru             # navigate across go code in editors
     golang.org/x/tools/cmd/gorename         # precise type-safe renaming
     golang.org/x/tools/cmd/goimports        # drop in replacement for gofmt
-    github.com/nsf/gocode                   # autocompletion daemon
+
+    github.com/mdempsky/gocode              # autocompletion daemon
     github.com/k0kubun/pp                   # pretty printer
     github.com/motemen/gore                 # go REPL
     github.com/rogpeppe/godef               # find symbol information in go code
     github.com/mailgun/godebug              # cross-platform debugger
+    github.com/cweill/gotests/...
+    github.com/davidrjenni/reftools/cmd/fillstruct
+    github.com/fatih/gomodifytags
+    github.com/godoctor/godoctor
+    github.com/haya14busa/gopkgs/cmd/gopkgs
+    github.com/josharian/impl
+    github.com/zmb3/gogetdoc
 )
 
+CGO_ENABLED=0 go get -v -trimpath -ldflags '-s -w' github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -u -v "${packages[@]}"
