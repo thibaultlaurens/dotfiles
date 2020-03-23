3c5a3be7d4c8fef8afd2207b855da6f9e0f56a4e
Thibault Laurens <laurens.thibault@gmail.com>
2020-03-23 09:39:31 +0100

    ubuntu: update docker install script

--- pkg/docker.sh
+++ pkg/docker.sh
@@ -6,7 +6,13 @@ if [[ $(uname -s) == Darwin ]]; then
     brew cask install docker
     brew install docker-completion
 else
-    sudo apt install docker-ce
+    sudo apt-get remove docker docker-engine docker.io containerd runc
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
     systemctl start docker
     systemctl enable docker
     sudo usermod -aG docker thibault
