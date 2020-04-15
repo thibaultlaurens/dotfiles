3dfdbf5dca759009c23bc77a559ab76600009e4a
Thibault Laurens <laurens.thibault@gmail.com>
2020-04-15 14:31:13 +0200

    emacs python: go back to anaconda backend

--- lang/python.sh
+++ lang/python.sh
@@ -20,15 +20,14 @@ if [ ! -d "$PYENV_ROOT" ]; then
         brew install pyenv
     elif [[ $(uname) == "Linux" ]]; then
         curl https://pyenv.run | bash
-        exec "$SHELL"
     fi
 fi
 
-pyenv install 3.7.7
-pyenv global 3.7.7
-
 PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+
+pyenv install 3.7.7
+pyenv global 3.7.7
 
 echo "installing python packages..."
 
@@ -40,8 +39,8 @@ packages=(
     'importmagic'                 # find unresolved imports
     'ipython[all]'                # interactive python
     'isort'                       # sort imports
+    'mypy'                        # optional static typing checker
     'pip'                         # package installer
-    'python-language-server[all]' # language server protocol for python
     'setuptools'                  # build and distribute packages
     'virtualenv'                  # create isolated python environments
     'virtualenvwrapper'           # extensions for virtualenv
