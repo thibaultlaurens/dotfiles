42390175764e6d50dc84bcecc27864c5f87ff2e8
Thibault Laurens <laurens.thibault@gmail.com>
2020-04-28 19:20:39 +0200

    cleaner docker

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-echo "installing docker..."
+echo "installing docker.."
 
 if [[ $(uname) == "Darwin" ]]; then
     brew cask install docker
     brew install docker-completion
+
 elif [[ $(uname) == "Linux" ]]; then
     sudo apt remove docker docker-engine docker.io containerd runc
+
     sudo add-apt-repository \
        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
        $(lsb_release -cs) \
        stable"
-    sudo apt update
-    sudo apt install docker-ce docker-ce-cli containerd.io
-    systemctl start docker
+    sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io
+
     systemctl enable docker
+    systemctl start docker
     sudo usermod -aG docker tlaurens
 fi
+
+echo "done"
