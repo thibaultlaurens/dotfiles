a2221936d96f54dab8f235260181d5483cbd466e
Thibault Laurens <thibault.laurens@pm.me>
2020-10-11 13:14:33 +0200

    update emacs config

--- emacs/emacs.sh
+++ emacs/emacs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env/bash
 
 echo "installing emacs.."
+DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 if [[ $(uname) == "Darwin" ]]; then
     brew tap d12frosted/emacs-plus
@@ -20,18 +21,17 @@ mv .emacs .emacs.bak
 
 # install chemacs startup script
 wget -O ~/.emacs https://raw.githubusercontent.com/plexus/chemacs/master/.emacs
-ln -fs "$(pwd)/.emacs-profiles.el" "$HOME/.emacs-profiles.el"
+ln -fs "$DIR/.emacs-profiles.el" "$HOME/.emacs-profiles.el"
 
 # install spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/spacemacs
-ln -fs "$(pwd)/.spacemacs" "$HOME/.spacemacs"
+ln -fs "$DIR/.spacemacs" "$HOME/.spacemacs"
 
 # install doom-emacs
 git clone https://github.com/hlissner/doom-emacs ~/doom-emacs
-ln -fs "$(pwd)/.doom.d" "$HOME/.doom.d"
+ln -fs "$DIR/.doom.d" "$HOME/.doom.d"
 ~/doom-emacs/bin/doom install
 
 # use doom-emacs as default emacs
 echo 'doom' > "$HOME/.emacs-profile"
 
-echo "done"
