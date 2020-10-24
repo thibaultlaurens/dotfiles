4e78cf41d52a28cbae0d8498f1b28bb8bfd328e8
Thibault Laurens <thibault.laurens@pm.me>
2020-10-24 11:07:21 +0200

    go: cleanup go pkgs

--- lang/go.sh
+++ lang/go.sh
@@ -18,25 +18,21 @@ export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:/usr/local/go/bin
 
 packages=(
-    'golang.org/x/tools/cmd/godoc'          # extract and generate doc
-    'golang.org/x/tools/gopls'              # lsp server for go
-    'golang.org/x/tools/cmd/goimports'      # drop in replacement for gofmt
-    'golang.org/x/tools/cmd/gorename'       # precise type-safe renaming
-    'golang.org/x/tools/cmd/guru'           # navigate across go code in editors
+    # used by emacs golang layer 
+    'github.com/cweill/gotests/...'         # generate test code
+    'github.com/fatih/gomodifytags'         # tag manipulation
+    'github.com/motemen/gore/cmd/gore'      # repl
+    'github.com/stamblerre/gocode'          # code completion & eldoc support
+    'golang.org/x/tools/cmd/godoc'          # documentation lookup
+    'golang.org/x/tools/cmd/goimports'      # fmt on save and fix imports
+    'golang.org/x/tools/cmd/gorename'       # refactoring
+    'golang.org/x/tools/cmd/guru'           # code navigation and refactoring
     
-    'github.com/jessfraz/dockfmt'
-    'github.com/cweill/gotests/...'
-    'github.com/davidrjenni/reftools/cmd/fillstruct'
-    'github.com/fatih/gomodifytags'
-    'github.com/godoctor/godoctor'
-    'github.com/haya14busa/gopkgs/cmd/gopkgs'
-    'github.com/josharian/impl'
-    'github.com/k0kubun/pp'                 # pretty printer
-    'github.com/mailgun/godebug'            # cross-platform debugger
-    'github.com/stamblerre/gocode'          # autocompletion daemon
-    'github.com/motemen/gore/cmd/gore'      # go REPL
-    'github.com/rogpeppe/godef'             # find symbol information in go code
-    'github.com/zmb3/gogetdoc'
+    # used by emacs lsp layer 
+    'golang.org/x/tools/gopls'              # lsp server
+    
+    # used by emacs docker layer
+    'github.com/jessfraz/dockfmt'           # dockerfile fmt
 )
 
 echo "installing go packages.."
