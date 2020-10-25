85299f27df0707b56a1b5fa29ffb506789d4a0dc
Thibault Laurens <thibault.laurens@pm.me>
2020-10-25 20:48:34 +0100

    update base setup path

--- setup.sh
+++ setup.sh
@@ -1,50 +1,49 @@
 #!/usr/bin/env bash
 
 echo "setting up environment.."
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # OS related setup first
 if [[ $(uname) == "Darwin" ]]; then
-    source "$DIR/osx/setup.sh"
+    source "$DF_DIR/osx/setup.sh"
 elif [[ $(uname) == "Linux" ]]; then
-    source "$DIR/ubuntu/setup.sh"
+    source "$DF_DIR/ubuntu/setup.sh"
 fi
 
 # Install programming languages
-source "$DIR/lang/go.sh"
-source "$DIR/lang/node.sh"
-source "$DIR/lang/python.sh"
-source "$DIR/lang/rust.sh"
+source "$DF_DIR/lang/go.sh"
+source "$DF_DIR/lang/python.sh"
+source "$DF_DIR/lang/node.sh"
 
 # Install docker
-source "$DIR/docker/docker.sh"
+source "$DF_DIR/docker/docker.sh"
 
 # Install emacs
-source "$DIR/emacs/emacs.sh"
+source "$DF_DIR/emacs/emacs.sh"
 
 # Link bash config
-ln -fs "$DIR/bash/.bash_profile" "$HOME/.bash_profile"
-ln -fs "$DIR/bash/.inputrc" "$HOME/.inputrc"
-ln -fs "$DIR/bash/.bashrc" "$HOME/.bashrc"
+ln -fs "$DF_DIR/bash/.bash_profile" "$HOME/.bash_profile"
+ln -fs "$DF_DIR/bash/.inputrc" "$HOME/.inputrc"
+ln -fs "$DF_DIR/bash/.bashrc" "$HOME/.bashrc"
 
 # Link tmux config
-ln -fs "$DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
+ln -fs "$DF_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
 # Link git config
-ln -fs "$DIR/git/.gitconfig" "$HOME/.gitconfig"
-ln -fs "$DIR/git/.gitignore" "$HOME/.gitignore"
+ln -fs "$DF_DIR/git/.gitconfig" "$HOME/.gitconfig"
+ln -fs "$DF_DIR/git/.gitignore" "$HOME/.gitignore"
 
 # Link vim config
-ln -fs "$DIR/vim/.vimrc.plug" "$HOME/.vimrc.plug"
-ln -fs "$DIR/vim/.vimrc" "$HOME/.vimrc"
+ln -fs "$DF_DIR/vim/.vimrc.plug" "$HOME/.vimrc.plug"
+ln -fs "$DF_DIR/vim/.vimrc" "$HOME/.vimrc"
 
 # Link htop config
-ln -fs "$DIR/htop/htoprc" "${HOME}/.config/htop/htoprc"
+ln -fs "$DF_DIR/htop/htoprc" "${HOME}/.config/htop/htoprc"
 
 # Reload bashrc
 source "$HOME/.bashrc"
 
 echo "don't forget to copy the default ssh config"
-# cp "$DIR/ssh/config" "${HOME}/.ssh/config"
+# cp "$DF_DIR/ssh/config" "${HOME}/.ssh/config"
 
 echo "all done"
