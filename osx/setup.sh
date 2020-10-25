1a9395c7fd21b7801b854e1d19ffb1ae4c89b4af
Thibault Laurens <thibault.laurens@pm.me>
2020-10-25 20:47:28 +0100

    update osx base install path

--- osx/setup.sh
+++ osx/setup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo "setting up osx.."
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DF_OSX_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Install xcode cli
 xcode-select --install
 
 # Install packages and apps
-source "$DIR/brew.sh"
-source "$DIR/brew_cask.sh"
+source "$DF_OSX_DIR/brew.sh"
+source "$DF_OSX_DIR/brew_cask.sh"
 
 # Whitelist the upgraded shell
 echo /usr/local/bin/bash | sudo tee -a /etc/shells
@@ -18,13 +18,13 @@ chsh -s /usr/local/bin/bash
 sudo chsh -s /usr/local/bin/bash
 
 # Apply os preferences and hardening
-source "$DIR/preferences.sh"
-source "$DIR/hardening.sh"
+source "$DF_OSX_DIR/preferences.sh"
+source "$DF_OSX_DIR/hardening.sh"
 
 # Link Iterm2 config
-ln -fs "$DIR/iterm/com.googlecode.iterm2.plist" \
+ln -fs "$DF_OSX_DIR/iterm/com.googlecode.iterm2.plist" \
     "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
 
 # Link Menumeters config
-ln -fs "$DIR/menumeters/com.ragingmenace.MenuMeters.plist" \
+ln -fs "$DF_OSX_DIR/menumeters/com.ragingmenace.MenuMeters.plist" \
     "$HOME/Library/Preferences/com.ragingmenace.MenuMeters.plist"
