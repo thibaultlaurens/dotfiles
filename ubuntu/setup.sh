3110117b9c5894cf344aec99bd65fbcb75eb4b9c
Thibault Laurens <thibault.laurens@pm.me>
2020-11-17 09:38:36 +0100

    change font cloning dir

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -7,8 +7,9 @@ DF_UBUNTU_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 source "$DF_UBUNTU_DIR/apt.sh"
 
 # Install fonts
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts /tmp/nerd-fonts
-/tmp/nerd-fonts/install.sh SourceCodePro
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts "$HOME/nerd-fonts"
+"$HOME/nerd-fonts/install.sh" SourceCodePro
+rm -rf "$HOME/nerd-fonts"
 
 # Enable firewall
 sudo ufw enable
