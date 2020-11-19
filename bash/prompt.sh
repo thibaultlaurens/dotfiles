026510ae78adae3c4388985ec0936a2ae8d9f0bf
Thibault Laurens <thibault.laurens@pm.me>
2020-11-19 15:26:11 +0100

    update prompt

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -6,10 +6,10 @@ export GIT_PS1_SHOWUNTRACKEDFILES=1     # '%'=untracked
 export GIT_PS1_SHOWUPSTREAM="verbose"   # 'u='=no difference, 'u+1'=ahead by 1 commit
 
 function set_prompts {
-    local user_and_host="\[\033[01;34m\][\u@\h"
-    local location="\[\033[01;36m\]\w]"
+    local user_and_host="\[\033[01;34m\][\u@\h]"
+    local location="\[\033[01;36m\]\w"
     local ps1_color="\[\033[01;37m\]"
-    local tail="\[\033[01;34m\]$"
+    local tail="\[\033[01;34m\]>>>"
     local last_color="\[\033[00m\]"
 
     PS1="$user_and_host $location$ps1_color\$(__git_ps1) $tail$last_color "
