46e1981ab5bccb79eeef51e4cdad6f6f3f2babc1
Thibault Laurens <thibault.laurens@ssi.gouv.fr>
2021-07-02 17:12:42 +0200

    rust: add cargo-update crate

--- .install/rust.sh
+++ .install/rust.sh
@@ -14,12 +14,13 @@ fi
 source "$HOME/.cargo/env"
 
 packages=(
-    'exa'       # A modern replacement for ls
-    'procs'     # A modern replacement for ps
-    'du-dust'   # A modern replacement for du
-    'bandwhich' # Terminal bandwidth utilization tool
-    'fd-find'   # A modern replacement for find
-    'tealdeer'  # A very fast implementation of tldr
+    'exa'          # A modern replacement for ls
+    'procs'        # A modern replacement for ps
+    'du-dust'      # A modern replacement for du
+    'bandwhich'    # Terminal bandwidth utilization tool
+    'fd-find'      # A modern replacement for find
+    'tealdeer'     # A very fast implementation of tldr
+    'cargo-update' # Check and apply updates to installed executables
 )
 
 echo "installing rust packages.."
