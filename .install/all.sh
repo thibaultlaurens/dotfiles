1d4de4213d0d0988018d6a6f13b7af83944dca71
Thibault Laurens <thibault.laurens@pm.me>
2021-08-06 16:52:40 +0200

    refresh install all script

--- .install/all.sh
+++ .install/all.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+# Apply os hardening
+source "$HOME/.install/harden.sh"
+
 # Install xcode cli
 xcode-select --install
 
 # Install packages
 source "$HOME/.install/brew.sh"
 
-# Apply os hardening and preferences
-source "$HOME/.install/macos_harden.sh"
-source "$HOME/.install/macos_prefs.sh"
+# Setup shell and gpg
+source "$HOME/.install/shell.sh"
+source "$HOME/.install/gpg.sh"
 
 # Install programming languages
 source "$HOME/.install/go.sh"
@@ -17,8 +20,3 @@ source "$HOME/.install/node.sh"
 
 # Install emacs
 source "$HOME/.install/emacs.sh"
-
-# Setup zsh, install starship prompt and omz
-chsh -s /usr/local/bin/zsh
-curl -fsSL https://starship.rs/install.sh | bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
