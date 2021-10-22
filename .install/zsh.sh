454b1b7faf5995fc2ccf220ca09dc71cada618e9
Thibault Laurens <thibault.laurens@pm.me>
2021-10-23 00:57:33 +0200

    update config

--- .install/zsh.sh
+++ .install/zsh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Install zsh 
+# Install zsh
 brew install zsh
 chsh -s /usr/local/bin/zsh
 
@@ -10,11 +10,10 @@ config restore "$HOME/.zshrc"
 
 # Clone zsh plugins
 : "${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}"
-git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
+git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "$ZSH_CUSTOM"/plugins/autoupdate
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM"/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions "$ZSH_CUSTOM"/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM"/plugins/zsh-syntax-highlighting
 
 # Reload zshrc
 source "$HOME/.zshrc"
-
