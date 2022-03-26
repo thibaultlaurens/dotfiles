d6d466e8be44a607f1781266d920caa52bff9b7f
Thibault Laurens <thibault.laurens@pm.me>
2022-03-26 08:35:53 +0100

    update config

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -81,11 +81,11 @@
        grammar                     ; tasing grammar mistake every you make
 
        :tools
-       ansible                     ; a crucible for infrastructure as code
+       ;; ansible                  ; a crucible for infrastructure as code
        debugger                    ; FIXME stepping through code, to help you add bugs
        ;;direnv                    ; be direct about your environment
        (docker +lsp)               ; port everything to containers
-       editorconfig                ; let someone else argue about tabs vs spaces
+       ;; editorconfig             ; let someone else argue about tabs vs spaces
        ;;ein                       ; tame Jupyter notebooks with emacs
        (eval +overlay)             ; run code, run (also, repls)
        ;;gist                      ; interacting with github gists
@@ -98,7 +98,7 @@
        ;;prodigy                   ; FIXME managing external services & code builders
        rgb                         ; creating color strings
        ;;taskrunner                ; taskrunner for all your projects
-       terraform                   ; infrastructure as code
+       ;; terraform                ; infrastructure as code
        ;;tmux                      ; an API for interacting with tmux
        ;;upload                    ; map local to remote projects via ssh/ftp
 
@@ -131,7 +131,7 @@
        ;;hy                        ; readability of scheme w/ speed of python
        ;;idris
        (json +lsp)                 ; At least it ain't XML
-       (java +meghanada)           ; the poster child for carpal tunnel syndrome
+       ;; (java +meghanada)        ; the poster child for carpal tunnel syndrome
        (javascript +lsp)           ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia                     ; a better, faster MATLAB
        ;;kotlin                    ; a better, slicker Java(Script)
